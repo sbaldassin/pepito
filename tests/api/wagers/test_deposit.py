49c34c25c523992da956d29081b9ef1c6f490bb3
Santiago Baldassin <sbaldassin@gmail.com>
2019-04-28 22:38:29 -0300

    fixing

--- tests/api/wagers/test_deposit.py
+++ tests/api/wagers/test_deposit.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import logging
 from unittest import TestCase
@@ -9,8 +10,7 @@ from tests.factory.player_factory import create_random_player
 from tests.factory.revenue_factory import create_revenue
 from tests.utils.generator import generate_random_int
 from tests.utils.getters import get_until_not_empty
-from tests.utils.utils import get_player_sign_up_resource, get_api_headers, get_revenue_resource, \
-    get_deposit_resource
+from tests.utils.utils import get_player_sign_up_resource, get_api_headers, get_deposit_resource
 
 logging.basicConfig(level=logging.INFO)
 
@@ -21,7 +21,7 @@ class DepositTestCase(TestCase):
         super(DepositTestCase, self)
 
     def test_tc_1_player_revenue(self):
-        player, deposit = self._create_player_with_revenue()
+        player, revenues = self._create_player_with_revenue()
         result = self.get_revenue_from_db(player)
         logging.info("DB result: {}".format(result))
 
@@ -30,18 +30,18 @@ class DepositTestCase(TestCase):
 
     def test_tc_2_player_revenue_with_eur_currency(self):
         revenue = create_revenue()
-        revenue.currency = "EUR"
-        player, revenue = self._create_player_with_revenue(revenue)
+        revenue.Currency = "EUR"
+        player, revenue = self._create_player_with_revenue([revenue])
         result = self.get_revenue_from_db(player)
         logging.info("DB result: {}".format(result))
 
         self.assertFalse(result == [])
-        self.assertEquals(revenue.amount, result[0]["Amount"])
+        self.assertEquals(int(revenue.Amount) * 100, result[0]["EuroAmount"])
 
     def test_tc_3_player_revenue_with_invalid_currency(self):
         revenue = create_revenue()
-        revenue.currency = 123
-        player, revenue = self._create_player_with_revenue(revenue)
+        revenue.Currency = 123
+        player, revenue = self._create_player_with_revenue([revenue])
         result = self.get_revenue_from_db(player)
         logging.info("DB result: {}".format(result))
 
@@ -49,8 +49,8 @@ class DepositTestCase(TestCase):
 
     def test_tc_4_player_revenue_with_zero_amount(self):
         revenue = create_revenue()
-        revenue.amount = 0
-        player, revenue = self._create_player_with_revenue(revenue)
+        revenue.Amount = 0
+        player, revenue = self._create_player_with_revenue([revenue])
         result = self.get_revenue_from_db(player)
         logging.info("DB result: {}".format(result))
 
@@ -58,8 +58,8 @@ class DepositTestCase(TestCase):
 
     def test_tc_5_player_revenue_with_negative_amount(self):
         revenue = create_revenue()
-        revenue.amount = -1
-        player, revenue = self._create_player_with_revenue(revenue)
+        revenue.Amount = -1
+        player, revenue = self._create_player_with_revenue([revenue])
         result = self.get_revenue_from_db(player)
         logging.info("DB result: {}".format(result))
 
@@ -67,8 +67,8 @@ class DepositTestCase(TestCase):
 
     def test_tc_6_player_revenue_with_string_amount(self):
         revenue = create_revenue()
-        revenue.amount = "sdk"
-        player, revenue = self._create_player_with_revenue(revenue)
+        revenue.Amount = "sdk"
+        player, revenue = self._create_player_with_revenue([revenue])
         result = self.get_revenue_from_db(player)
         logging.info("DB result: {}".format(result))
 
@@ -76,8 +76,8 @@ class DepositTestCase(TestCase):
 
     def test_tc_7_player_revenue_with_future_date(self):
         revenue = create_revenue()
-        revenue.transaction_date = datetime.datetime(2030, 4, 24, 18, 26, 1, 37000).strftime('%Y-%m-%d')
-        player, revenue = self._create_player_with_revenue(revenue)
+        revenue.TransactionDate = datetime.datetime(2030, 4, 24, 18, 26, 1, 37000).strftime('%Y-%m-%d')
+        player, revenue = self._create_player_with_revenue([revenue])
         result = self.get_revenue_from_db(player)
         logging.info("DB result: {}".format(result))
 
@@ -85,12 +85,22 @@ class DepositTestCase(TestCase):
 
     def test_tc_8_player_revenue_with_invalid_date(self):
         revenue = create_revenue()
-        revenue.transaction_date = "hah"
-        player, revenue = self._create_player_with_revenue(revenue)
+        revenue.TransactionDate = "hah"
+        player, revenue = self._create_player_with_revenue([revenue])
         result = self.get_revenue_from_db(player)
         logging.info("DB result: {}".format(result))
 
         self.assertTrue(result == [])
+
+    def test_tc_9_player_revenue_with_multiple_transactions(self):
+        revenue_1 = create_revenue()
+        revenue_2 = create_revenue()
+        player, revenues = self._create_player_with_revenue(revenues=[revenue_1, revenue_2])
+        result = self.get_revenue_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertFalse(result == [])
+        self.assertEquals(len(result), 2)
 
     @staticmethod
     def get_revenue_from_db(player):
@@ -98,7 +108,7 @@ class DepositTestCase(TestCase):
         return get_until_not_empty(url)
 
     @staticmethod
-    def _create_player_with_revenue(revenue=None):
+    def _create_player():
         player = create_random_player(player_id_length=40)
         logging.info("Creating player: {}".format(player.__dict__))
         response = requests.post(get_player_sign_up_resource(),
@@ -106,15 +116,24 @@ class DepositTestCase(TestCase):
                                  headers=get_api_headers())
         body = response.json()
         logging.info("API response: {}".format(body))
+        return player
 
-        revenue = create_revenue() if revenue is None else revenue
-        logging.info("Creating revenue: {}".format(revenue.__dict__))
+    @staticmethod
+    def _create_revenues(player, revenues=[]):
+        revenues = [create_revenue()] if revenues is [] else revenues
+        logging.info("Creating revenue: {}".format([r.__dict__ for r in revenues]))
 
-        data = {"PlayerID": player.PlayerID, "InitID": generate_random_int(), "Transactions": [revenue.__dict__]}
+        data = {"PlayerID": player.PlayerID, "InitID": generate_random_int(),
+                "Transactions": [r.__dict__ for r in revenues]}
         logging.info("Request data: {}".format(json.dumps(data)))
         response = requests.post(get_deposit_resource(),
                                  data=json.dumps(data),
                                  headers=get_api_headers())
 
         logging.info("API response: {}".format(response.json()))
-        return player, revenue
+        return revenues
+
+    def _create_player_with_revenue(self, revenues=[]):
+        player = self._create_player()
+        revenues = self._create_revenues(player, revenues)
+        return player, revenues
--- tests/factory/revenue_factory.py
+++ tests/factory/revenue_factory.py
@@ -5,7 +5,7 @@ from tests.utils.generator import generate_random_int, generate_random_date
 def create_revenue():
     revenue = Revenue(
         amount=generate_random_int(),
-        currency="USD",
+        currency="EUR",
         transaction_date=generate_random_date(),
         success=True,
         payment_method="Skrill")
