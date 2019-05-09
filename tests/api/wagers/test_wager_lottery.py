cf78dab853b8ea1a04b2b3384641420c08ccb07e
Santiago Baldassin <sbaldassin@gmail.com>
2019-05-08 23:16:41 -0300

    more tests

--- tests/api/wagers/test_wager_lottery.py
+++ tests/api/wagers/test_wager_lottery.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import logging
 from unittest import TestCase
@@ -17,7 +18,7 @@ class WagerLotteryTestCase(TestCase):
     def setUp(self):
         super(WagerLotteryTestCase, self)
 
-    def test_tc_1_player_wager(self):
+    def test_tc_1_player_wager_lottery(self):
         player, wager = self._create_player_with_wager()
         result = self.get_wager_from_db(player)
         logging.info("DB result: {}".format(result))
@@ -25,6 +26,140 @@ class WagerLotteryTestCase(TestCase):
         self.assertFalse(result == [])
         self.assertTrue(result[0]["EuroCentsValue"] != 0)
 
+    def test_tc_2_wager_lottery_invalid_currency(self):
+        wager = create_wager_lottery()
+        wager.Currency = "Invalid"
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertTrue(result == [])
+
+    def test_tc_3_wager_lottery_usd_currency(self):
+        wager = create_wager_lottery()
+        wager.Currency = "USD"
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertFalse(result == [])
+
+    def test_tc_4_wager_lottery_invalid_value(self):
+        wager = create_wager_lottery()
+        wager.Value = "Invalid"
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertTrue(result == [])
+
+    def test_tc_5_wager_lottery_negative_value(self):
+        wager = create_wager_lottery()
+        wager.Value = -1
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertTrue(result == [])
+
+    def test_tc_6_wager_lottery_zero_value(self):
+        wager = create_wager_lottery()
+        wager.Value = 0
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertTrue(result == [])
+
+    def test_tc_7_wager_lottery_huge_value(self):
+        wager = create_wager_lottery()
+        wager.Value = 100000000000
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertTrue(result == [])
+
+    def test_tc_8_wager_lottery_zero_count(self):
+        wager = create_wager_lottery()
+        wager.Count = 0
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertTrue(result == [])
+
+    def test_tc_9_wager_lottery_negative_count(self):
+        wager = create_wager_lottery()
+        wager.Count = -1
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertTrue(result == [])
+
+    def test_tc_10_wager_lottery_invalid_count(self):
+        wager = create_wager_lottery()
+        wager.Count = "invalid"
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertTrue(result == [])
+
+    def test_tc_11_wager_lottery_huge_count(self):
+        wager = create_wager_lottery()
+        wager.Count = 100000000000
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertTrue(result == [])
+
+    def test_tc_12_wager_lottery_invalid_category(self):
+        wager = create_wager_lottery()
+        wager.Category = "invalid"
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertTrue(result == [])
+
+    def test_tc_13_wager_lottery_invalid_transaction_date(self):
+        wager = create_wager_lottery()
+        wager.TransactionDate = "invalid_date"
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertTrue(result == [])
+
+    def test_tc_14_wager_lottery_future_transaction_date(self):
+        wager = create_wager_lottery()
+        wager.TransactionDate = datetime.datetime(2030, 4, 24, 18, 26, 1, 37000).strftime('%Y-%m-%d')
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertTrue(result == [])
+
+    def test_tc_15_wager_lottery_invalid_draw_date(self):
+        wager = create_wager_lottery()
+        wager.DrawDate = "invalid_date"
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertTrue(result == [])
+
+    def test_tc_16_wager_lottery_future_draw_date(self):
+        wager = create_wager_lottery()
+        wager.DrawDate = datetime.datetime(2030, 4, 24, 18, 26, 1, 37000).strftime('%Y-%m-%d')
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertTrue(result == [])
 
     @staticmethod
     def get_wager_from_db(player):
--- tests/api/wagers/test_wager_sports.py
+++ tests/api/wagers/test_wager_sports.py
@@ -25,6 +25,96 @@ class WagerSportsTestCase(TestCase):
         self.assertFalse(result == [])
         self.assertTrue(result[0]["EuroCentsValue"] != 0)
 
+    def test_tc_2_wager_sport_invalid_currency(self):
+        wager = create_wager_sport()
+        wager.Currency = "Invalid"
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertTrue(result == [])
+
+    def test_tc_3_wager_sport_usd_currency(self):
+        wager = create_wager_sport()
+        wager.Currency = "USD"
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertFalse(result == [])
+
+    def test_tc_4_wager_sport_invalid_value(self):
+        wager = create_wager_sport()
+        wager.Value = "Invalid"
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertTrue(result == [])
+
+    def test_tc_5_wager_sport_negative_value(self):
+        wager = create_wager_sport()
+        wager.Value = -1
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertTrue(result == [])
+
+    def test_tc_6_wager_sport_zero_value(self):
+        wager = create_wager_sport()
+        wager.Value = 0
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertTrue(result == [])
+
+    def test_tc_7_wager_sport_huge_value(self):
+        wager = create_wager_sport()
+        wager.Value = 100000000000
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertTrue(result == [])
+
+    def test_tc_8_wager_sport_zero_count(self):
+        wager = create_wager_sport()
+        wager.Count = 0
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertTrue(result == [])
+
+    def test_tc_9_wager_sport_negative_count(self):
+        wager = create_wager_sport()
+        wager.Count = -1
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertTrue(result == [])
+
+    def test_tc_10_wager_sport_invalid_count(self):
+        wager = create_wager_sport()
+        wager.Count = "invalid"
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertTrue(result == [])
+
+    def test_tc_11_wager_sport_huge_count(self):
+        wager = create_wager_sport()
+        wager.Count = 100000000000
+        player, revenue = self._create_player_with_wager([wager])
+        result = self.get_wager_from_db(player)
+        logging.info("DB result: {}".format(result))
+
+        self.assertTrue(result == [])
+
     @staticmethod
     def get_wager_from_db(player):
         url = "http://{}/wagers?customer_id={}".format(get_config().get("test_framework", "db"), player.PlayerID)
