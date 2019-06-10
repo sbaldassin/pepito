1f7a64193436d15741ac51f188f4ad5c5d137f66
Santiago Baldassin <sbaldassin@gmail.com>
2019-06-10 10:28:51 -0300

    fixing product ids

--- tests/factory/payout_factory.py
+++ tests/factory/payout_factory.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from tests.models.game import CasinoGame, SportGame, BetGame, EsportGame, LotteryGame, ParimutuelGame
 from tests.models.payout import Payout
 from tests.utils.generator import generate_random_int, generate_random_date
@@ -6,7 +8,7 @@ from tests.utils.generator import generate_random_int, generate_random_date
 def create_payout(payout_type, **kwargs):
     payout = Payout(
         amount=float(generate_random_int(3)),
-        product_id=int(generate_random_int(2)),
+        product_id=game_ids[payout_type],
         currency="EUR",
         transaction_date=generate_random_date(include_time=True),
         game=game_types[payout_type]().__dict__,
@@ -23,3 +25,12 @@ game_types = {
     "parimutuel": ParimutuelGame
 }
 
+
+game_ids = {
+    "casino": 1,
+    "sport": 2,
+    "bet": 3,
+    "esport": 4,
+    "lottery": 5,
+    "parimutuel": 6
+}
