9f8312eafa93949cb239dd74042158d058f8ddbd
vahidjazbani <vahidjzbn@gmail.com>
2019-06-10 10:20:50 -0400

    Update withdrawal.py
    Fix for Transaction Date

--- tests/models/withdrawal.py
+++ tests/models/withdrawal.py
@@ -5,6 +5,6 @@ class Withdrawal:
     def __init__(self, amount, currency, transaction_date):
         self.amount = amount
         self.currency = currency
-        self.transaction_date = transaction_date
+        self.TransactionDate = transaction_date
 
 
