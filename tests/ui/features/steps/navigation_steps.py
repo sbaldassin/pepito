05cc42a5a8670f6d7e8142b4bd8a5a2f2d1a97a9
Juan Manuel Figueroa <juanma@thirdlove.com>
2019-07-16 01:30:37 -0300

    Adding integration errors feature.

--- tests/ui/features/steps/navigation_steps.py
+++ tests/ui/features/steps/navigation_steps.py
@@ -1,12 +1,19 @@
 from behave import step
 
 from tests.ui.config.config import get_config
+from tests.ui.page_objects.home_page import HomePage
 
 
 @step("I navigate to the home page")
 def navigate_to_home(context):
     context.browser.get(get_config().get("webapp", "url"))
 
+
 @step("I navigate to the dimensions data page")
 def navigate_to_dimensions_data(context):
     context.browser.get(get_config().get("webapp", "dimensions"))
+
+
+@step("I click in integration errors button")
+def navigate_to_integration_errors(context):
+    context.browser.find_element(*HomePage.integration_errors_btn_locator).click()
--- tests/ui/page_objects/home_page.py
+++ tests/ui/page_objects/home_page.py
@@ -7,3 +7,7 @@ class HomePage:
     login_form_locator = (By.CLASS_NAME, "login-form")
     login_btn_locator = (By.TAG_NAME, "button")
     logged_in_name_locator = (By.ID, "spanUser")
+    integration_errors_btn_locator = (By.CLASS_NAME, "icon-exclamation")
+    integration_errors_badge_locator = (By.CSS_SELECTOR, ".badge.bg-orange-400")
+    campaign_notifications_btn_locator = (By.CLASS_NAME, "icon-alert")
+    campaign_notifications_badge_locator = (By.CSS_SELECTOR, ".badge.bg-theme")
