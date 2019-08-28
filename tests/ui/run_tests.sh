3294451e1ba7c9c8a9a7a9cb83200e489693726b
Daniel Quiroga <dquiroga@boelter.com>
2019-08-28 14:32:14 -0300

    fixing the way we run tests

--- tests/ui/run_tests.sh
+++ tests/ui/run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker-compose down 
-docker-compose up --build -d
+docker-compose -f tests/ui/docker-compose.yml down 
+docker-compose -f tests/ui/docker-compose.yml up --build -d
 sleep 20
-docker-compose run -e BROWSER=chrome test_runner ./run.sh
+docker-compose -f tests/ui/docker-compose.yml run -e BROWSER=chrome test_runner ./run.sh
