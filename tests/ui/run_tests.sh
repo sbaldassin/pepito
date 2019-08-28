311dfcbe558467891dc4d1dc313323578b4522e2
Daniel Quiroga <danielquiroga@Daniels-MacBook-Air.local>
2019-08-28 14:57:51 -0300

    adding a change absolute path to run.sh on run_tests.sh

--- tests/ui/run_tests.sh
+++ tests/ui/run_tests.sh
@@ -2,4 +2,4 @@
 docker-compose -f tests/ui/docker-compose.yml down 
 docker-compose -f tests/ui/docker-compose.yml up --build -d
 sleep 20
-docker-compose -f tests/ui/docker-compose.yml run -e BROWSER=chrome test_runner ./run.sh
+docker-compose -f tests/ui/docker-compose.yml run -e BROWSER=chrome test_runner ./tests/ui/run.sh
